26801ac399c2ab7e249c7130edc2ddbfeddb762d
Italo <contatoitallo@hotmail.com>
2022-09-15 14:57:43 -0300

    test: Implementa testes unitários  para cobrir parcialmente a camada de Service REQ-15

--- src/tests/unit/services/car.test.ts
+++ src/tests/unit/services/car.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import { ZodError } from 'zod';
+import { ErrorTypes } from '../../../errors/catalog';
 import CarModel from '../../../models/Cars';
 import CarService from '../../../services/Cars';
 import { carMock, carMockWithId } from '../mocks/carMock';
@@ -11,6 +12,11 @@ describe('Car Service', () => {
 
 	before(() => {
 		sinon.stub(carModel, 'create').resolves(carMockWithId);
+		sinon.stub(carModel, 'readOne')
+			.onCall(0).resolves(carMockWithId)
+			.onCall(1).resolves(null)
+			.onCall(2).resolves(carMockWithId)
+		sinon.stub(carModel, 'update').resolves(carMockWithId)
 	});
 
 	after(() => {
@@ -31,6 +37,30 @@ describe('Car Service', () => {
 				expect(error).to.be.instanceOf(ZodError);
 			}
 		});
+	});
+
+	describe('ReadOne Car', () => {
+		it('Success', async () => {
+			const carCreated = await carService.readOne(carMockWithId._id);
+
+			expect(carCreated).to.be.deep.equal(carMockWithId);
+		});
+
+		it('Failure', async () => {
+			try {
+				await carService.readOne(carMockWithId._id);
+			} catch (error: any) {
+				expect(error.message).to.be.deep.equal(ErrorTypes.ErrorNotFound);
+			}
+		});
+	});
+
+	describe('Update Car', () => {
+		it('Success', async () => {
+			const cars = await carService.update('62cf1fc6498565d94eba52cd', carMock);
+			expect(cars).to.be.deep.equal(carMockWithId);
+		});
+
 	});
 
 });
